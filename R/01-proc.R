d963b6d17a1bf643ba1661d97d0fae5dcd4d50fc
José Miguel Valdés <jose17valdes@gmail.com>
2025-03-21 12:28:05 -0300

    Carga de paquetes para Regresión Múltiple

--- R/01-proc.R
+++ R/01-proc.R
@@ -1,5 +1,24 @@
 #Curso: Repaso 1
 
-#Sociologos: Maximiliano Levenier, Mistian Herrera y José Valdés
+#Sociólogos: Maximiliano Levenier, Mistian Herrera y José Valdés
+
+# Trabajo: Regresión Múltiple
+
+#Desactivar notación científica
+
+options(scipen = 999)
+
 
 #Cargar paquetes
+
+pacman::p_load(tidyverse, # Incluye dplyr para la manipulación de datos y haven para importar datos
+               sjmisc,    # Para explorar y transformar datos
+               sjPlot,    # Para analizar datos (mostrar resultados)
+               lmtest,    # Para realizar la validez de los modelos de regresión (heterocedasticidad)
+               car)       # Para el diagnóstico de regresión y pruebas estadísticas (vif)
+
+
+
+
+#Cargar datos (Maxi o Mitam)
+
